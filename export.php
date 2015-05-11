5c04125a449c9234dc7fdd028a48dc3d160535fc
MeLlamoPablo <pabloviolin8@gmail.com>
2015-05-11 18:34:27 +0200

    Exporter.php now outputs a single exam as JSON

--- export.php
+++ export.php
@@ -12,10 +12,29 @@ if(isset($_GET['examen'])){
 	//Single export
 
 	//Get data from examenes
+	$resultado = $mysqli->query("SELECT * FROM examenes WHERE id = ".$_GET['examen']);
+	$examen = $resultado->fetch_assoc();
 
-	$export = array('title' => , );
+	//Get data from preguntas
+	$questions = array();
+	$resultado = $mysqli->query("SELECT * FROM preguntas WHERE examen = ".$_GET['examen']);
+	while($preguntas = $resultado->fetch_assoc()){
+		$questions[$preguntas['num'] + 1] = array(
+			'num' => $preguntas['num'],
+			'lang1' => $preguntas['esp'],
+			'lang2' => $preguntas['fra'],
+			'mode' => $preguntas['modo']);
+	}
+
+	$export = array(
+		'title' => $examen['nombre'],
+		'active' => $examen['activa'],
+		'numberOfQuestions' => $examen['preguntas'],
+		'questions' => $questions);
+
+	echo json_encode($export); //TODO download into a file named foo.json
 }else{
-	//Export all
+	//TODO Export all
 }
 
 ?>
